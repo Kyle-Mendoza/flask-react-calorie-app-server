f6f6ccfd62970dfbd261ca47398e9df3321395de
Kyle-Mendoza <kimmendoza0131@gmail.com>
2025-04-08 14:33:29 +0800

    reinstall venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kyle-mendoza/workspace/personal/flask-react-calorie-app-server/.venv"
+set -gx VIRTUAL_ENV "/Users/kael/Documents/MyCode/flask-react-calorie-app-server/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
